0288f1ca137170e67c04988d5c2f3472e4fcb828
ShreyanshPachouri <165273155+ShreyanshPachouri@users.noreply.github.com>
2024-06-16 21:58:42 +0530

    Update product.js

--- product.js
+++ product.js
@@ -16,7 +16,7 @@ const productSchema = new mongoose.Schema({
         type: String,
         enum: ["Fruit", "Vegetable", "Dairy"]
     }
-
+//This is a javaScript object. The keys in this object(name, price, category) will be used as keys for storing data in the Mongo database. Here we are defining that the data returned from the database should be of a specific type. For example, name should be a string, price should be a number etc.
 })
 
 const Product = mongoose.model("Product", productSchema)
